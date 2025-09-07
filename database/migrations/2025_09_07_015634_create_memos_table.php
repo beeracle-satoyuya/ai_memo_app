8019b4bbabb1a879560d91cdd72e84c354f2f4cb
beeracle-satoyuya <official@beracle.jp>
2025-09-07 12:02:21 +0900

    詳細画面完成

--- database/migrations/2025_09_07_015634_create_memos_table.php
+++ database/migrations/2025_09_07_015634_create_memos_table.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('memos', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('title');
             $table->text('content');
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
